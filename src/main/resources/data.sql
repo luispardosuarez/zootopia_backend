d2698a6e78509c2a476cd2a2bed00b8ff348bd66
Luis Pardo <luispardosuarez@gmail.com>
2024-01-24 15:10:37 +0100

    check data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO animals("id", "name", "type", "family", "gender", "date") VALUES(default, 'Felix', 'lince', 'félidos', 'M', '2023/01/01');
-INSERT INTO animals("id", "name", "type", "family", "gender", "date") VALUES(default, 'Felisa', 'lince', 'félidos', 'H', '2023/01/01');
+INSERT INTO animals(id, name, type, family, gender, date) VALUES(default, 'Felix', 'lince', 'félidos', 'M', '2023/01/01');
+INSERT INTO animals(id, name, type, family, gender, date) VALUES(default, 'Felisa', 'lince', 'félidos', 'H', '2023/01/01');
